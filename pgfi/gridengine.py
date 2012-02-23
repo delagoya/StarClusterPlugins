d03ac376ba1986115c75b382f389768031ffa7ee
Angel Pizarro <angel@turaro.com>
2012-02-22 22:30:49 -0500

    small bug fix for sge_request file

--- pgfi/gridengine.py
+++ pgfi/gridengine.py
@@ -17,9 +17,9 @@ class GridEngineTweaks(ClusterSetup):
         log.debug("enable_hvmem = %s , master_slots = %s" % (self.enable_hvmem, self.master_slots))
 
     def run(self, nodes, master, user, user_shell, volumes):
-        self.set_master_slots(master,nodes)
         if self.enable_hvmem:
             self.enable_hvmem_f(master,nodes)
+        self.set_master_slots(master,nodes)
 
     def set_master_slots(self,master,nodes):
         log.info("Setting the number of slots on master to %s" % self.master_slots)
@@ -31,15 +31,14 @@ class GridEngineTweaks(ClusterSetup):
         master.ssh.execute("sed -i.bak -E \"s/^(h_vmem.*)/h_vmem  h_vmem   MEMORY <=  YES  YES 1g 0/\" /tmp/complex.conf")
         master.ssh.execute("qconf -Mc /tmp/complex.conf", source_profile=True)
         log.info("Setting sge_request defaults")
-        default_sge_params = '''
--q all.q
+        default_sge_params = '''-q all.q
 -l h_vmem=1g
 -l h_stack=256m
-        '''
+'''
         master.ssh.execute('echo "' + default_sge_params + '"> $SGE_ROOT/$SGE_CELL/common/sge_request', source_profile=True)
         # set the slots and h_vmem for the nodes
         for node in nodes:
             atts = (node.num_processors, node.memory, node.alias)
             if master.alias == node.alias:
                 atts = (self.master_slots, master.memory, master.alias)
-            master.ssh.execute("qconf -rattr exechost complex_values slots=%s,h_vmem=%s %s" % atts, source_profile=True)
+            master.ssh.execute("qconf -rattr exechost complex_values slots=%s,h_vmem=%sm %s" % atts, source_profile=True)
